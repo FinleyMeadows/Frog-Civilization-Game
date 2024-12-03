2882afa4cd93c2d855fb75e19c3d4e10ca3c452a
FinleyMeadows <finleytmeadows@gmail.com>
2024-12-02 18:25:04 -0800

    Set up the very basics of the layout (WITH COMMENTS)

--- GameRunner.java
+++ GameRunner.java
@@ -1,5 +1,6 @@
 public class GameRunner {
     public static void main(String[] args) {
-
+        // creates the window and starts the game
+        new Pond();
     }
 }
--- Pond.java
+++ Pond.java
@@ -1,3 +1,36 @@
-public class Pond {
+import javax.swing.*;
+import java.awt.*;
 
+public class Pond extends JFrame {
+
+    // frame's width
+    private final int FRAME_WIDTH = 700;
+    // frame's height
+    private final int FRAME_HEIGHT = 700;
+
+    // holds every component in the game
+    private JLayeredPane layeredPane;
+
+    // keep track of the number of frogs alive in the pond
+    private int numFrogs = 4;
+
+    // contains all the functions of the game
+    public Pond() {
+        // IMPORTANT: this = the JFrame
+
+        // basic JFrame methods
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        // sets layout so that the pack() method works
+        this.setLayout(new BorderLayout());
+        this.setVisible(true);
+
+        // JLayeredPane methods
+        layeredPane = new JLayeredPane();
+        // the pack() method resizes to preferred sizes not normal sizes
+        layeredPane.setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
+        // adds the layered pane to the center of the JFrame
+        this.add(layeredPane, BorderLayout.CENTER);
+        // resizes the JFrame to the size of layeredPane
+        this.pack();
+    }
 }
