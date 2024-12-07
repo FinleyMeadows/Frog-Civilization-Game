18ef7665c156e415d21fc7a4725c7d01df4390f5
FinleyMeadows <finleytmeadows@gmail.com>
2024-12-06 23:39:47 -0800

    Added Frog class and did some time label formatting

--- Pond.java
+++ Pond.java
@@ -5,38 +5,58 @@
 */
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
-public class Pond extends JFrame {
+public class Pond extends JFrame implements MouseListener {
 
-    // Frame's width
+    // - - - V A R I A B L E S - - - //
     private final int FRAME_WIDTH = 700;
-    // Frame's height
     private final int FRAME_HEIGHT = 700;
+
+    // holds the current hour of the day
+    private int time = 8;
+    // the time when the day ends
+    private int bedtime = 8;
+    private String amOrPm = "am";
+    // 1-100, 1 being nasty, 100 being crystal clear
+    public static int waterCleanliness = 100;
 
     // - - - C O M P O N E N T S - - - //
 
-    // Holds every component in the game
+    // background labels
+    private JLabel sky = new JLabel(new ImageIcon("Pictures/Sky.png"));
+    private JLabel water = new JLabel(new ImageIcon("Pictures/Water.png"));
+    private JLabel ground = new JLabel(new ImageIcon("Pictures/Ground.png"));
+
+    // time variables and components
+    private JLabel timeLabel = new JLabel();
+    private JLabel bedtimeLabel = new JLabel();
+    private JLabel nextDayLabel = new JLabel();
+
+    // holds every component in the game
     private JLayeredPane layeredPane;
 
     // - - - M A P S - - - //
 
     // holds every type of item and its quantity
-    private Map<String, Integer> items = new HashMap<String, Integer>();
+    private Map<String, Integer> items = new TreeMap<String, Integer>();
 
     // - - - A R R A Y L I S T S - - - //
 
-    // Stores all the display labels
+    // stores all the display labels
     private ArrayList<JLabel> displayLabels = new ArrayList<JLabel>();
 
-    // Contains all the functions of the game
+    // contains all the functions of the game
     public Pond() {
         // IMPORTANT: this = the JFrame
 
-        // Basic JFrame methods
+        // basic JFrame methods
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         // Sets layout so that the pack() method works
         this.setLayout(new BorderLayout());
@@ -46,17 +66,26 @@ public class Pond extends JFrame {
         layeredPane = new JLayeredPane();
         // The pack() method resizes to preferred sizes not normal sizes
         layeredPane.setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
-        // Adds the layered pane to the center of the JFrame
+        // adds the layered pane to the center of the JFrame
         this.add(layeredPane, BorderLayout.CENTER);
-        // Resizes the JFrame to the size of layeredPane
+        // resizes the JFrame to the size of layeredPane
         this.pack();
+
+        // creates each chunk of the background
+        createBackground();
+        // creates every type of item and sets its value
+        initializeItems();
+        // creates a display label for every kind of item and adds it to the frame
+        initializeItemDisplayLabels();
+        // create the time and time functions in the top right of the frame
+        initializeTimeComponents();
     }
 
     // - - - I T E M  M A N A G E M E N T - - - //
 
-    // Initializes each item and its quantity
+    // initializes each item and its quantity
     public void initializeItems() {
-        // Number of frogs, tadpoles, bugs, plant food, and hours spent in total
+        // number of frogs, tadpoles, bugs, plant food, and hours spent in total
         items.put("Frogs", 4);
         items.put("Tadpoles", 0);
         items.put("Bugs", 0 /*TBD*/);
@@ -64,33 +93,200 @@ public class Pond extends JFrame {
         items.put("Total Hours Spent", 0);
     }
 
-    // Creates a display label for each item and positions them accordingly in the frame
+    // creates a display label for each item and positions them accordingly in the frame
     public void initializeItemDisplayLabels() {
-        // Creates a display label for each item in the items map<String, Integer>
+        int yPos = 0;
+
+        // creates a display label for each item in the items map<String, Integer>
         for (Map.Entry<String, Integer> entry : items.entrySet()) {
             String name = entry.getKey();
             int quantity = entry.getValue();
 
+            // customizing the display label
             JLabel displayLabel = new JLabel();
+            displayLabel.setFont(new Font("Serif", Font.PLAIN, 20));
             displayLabel.setText(name + ": " + quantity);
-            // TODO: Figure out sizing and positioning
-            // displayLabel.setSize();
-            // displayLabel.setLocation();
+            displayLabel.setSize(displayLabel.getPreferredSize().width + 10, displayLabel.getPreferredSize().height);
+            System.out.println(displayLabel.getPreferredSize());
+            displayLabel.setLocation(0, yPos);
 
-            // Adds the displayLabel to the displayLabels ArrayList for future access
+            // adds the displayLabel to the displayLabels ArrayList for future access
             displayLabels.add(displayLabel);
+
+            // adds it to the layeredPane
+            layeredPane.add(displayLabel, Integer.valueOf(1));
+
+            // moves the yPos down below the previous label
+            yPos += displayLabel.getHeight();
         }
     }
 
-    // Updates all display labels whenever necessary
+    // updates all resource display labels whenever necessary
     public void updateDisplayLabels() {
-        // Updates the text of all the display labels to match their actual quantities in the map
+        // updates the text of all the display labels to match their actual quantities in the map
         for (JLabel displayLabel : displayLabels) {
             String text = displayLabel.getName();
             String itemName = text.substring(0, text.indexOf(":"));
 
-            // Resets the label
+            // resets the label
             displayLabel.setText(itemName + ": " + items.get(itemName));
+            displayLabel.setSize(displayLabel.getPreferredSize());
+        }
+    }
+
+
+    // - - - T I M E  M A N A G E M E N T - - - //
+
+
+    public void initializeTimeComponents() {
+        int width = 110;
+
+        // formats the time label
+        timeLabel.setFont(new Font("Dialog", Font.PLAIN, 30));
+        // updates the text in the time label
+        updateTime();
+        timeLabel.setSize(width, timeLabel.getPreferredSize().height);
+        timeLabel.setLocation(FRAME_WIDTH - width, 0);
+        // adds it to the frame
+        layeredPane.add(timeLabel, Integer.valueOf(1));
+
+        // formats the bedtime label
+        bedtimeLabel.setFont(new Font("Serif", Font.PLAIN, 15));
+        bedtimeLabel.setText("Bedtime: " + bedtime + ":00pm");
+        bedtimeLabel.setSize(width, timeLabel.getPreferredSize().height);
+        bedtimeLabel.setLocation(FRAME_WIDTH - width, timeLabel.getY() + timeLabel.getHeight());
+        // adds it to the frame
+        layeredPane.add(bedtimeLabel, Integer.valueOf(1));
+
+        // formats next day button
+        nextDayLabel.setName("Next Day Label");
+        nextDayLabel.setFont(new Font("Serif", Font.PLAIN, 20));
+        nextDayLabel.setText("Next Day");
+        // moves to next day when clicked
+        nextDayLabel.addMouseListener(this);
+        nextDayLabel.setSize(width, timeLabel.getPreferredSize().height);
+        nextDayLabel.setLocation(FRAME_WIDTH - width, bedtimeLabel.getY() + bedtimeLabel.getHeight());
+        // adds it to the frame
+        layeredPane.add(nextDayLabel, Integer.valueOf(1));
+    }
+
+    // updates the time label
+    public void updateTime() {
+        // time == 12am or pm
+        if (time == 12) {
+            if (amOrPm.equals("am")) {
+                timeLabel.setText(time + ":00pm");
+            }
+            else {
+                timeLabel.setText(time + ":00am");
+            }
+        }
+        // time != 12am or pm
+        else {
+            if (time > 12) {
+                // Ex: time = 13pm -> 1pm
+                time -= 12;
+                // flip-flop am and pm
+                if (amOrPm.equals("am")) {
+                    amOrPm = "pm";
+                }
+                else {
+                    amOrPm = "am";
+                }
+            }
+            timeLabel.setText(time + ":00" + amOrPm);
+        }
+    }
+
+
+    // - - - B A C K G R O U N D - - - //
+
+    public void createBackground() {
+        // adds the SKY to the background
+        sky.setName("Sky");
+        sky.setSize(sky.getPreferredSize());
+        sky.addMouseListener(this);
+        // layeredPane.add(sky, Integer.valueOf(0));
+
+        // adds the WATER to the background
+        water.setName("Water");
+        water.setSize(water.getPreferredSize());
+        water.setLocation(0, sky.getHeight());
+        water.addMouseListener(this);
+        // layeredPane.add(water, Integer.valueOf(0));
+
+        // adds the GROUND to the background
+        ground.setName("Ground");
+        ground.setSize(ground.getPreferredSize());
+        ground.setLocation(0, water.getY() + water.getHeight());
+        ground.addMouseListener(this);
+        // layeredPane.add(ground, Integer.valueOf(0));
+
+        // TODO: remove this once done with recreating each individual background label
+        JLabel background = new JLabel(new ImageIcon("Pictures/PondBackground.png"));
+        background.setSize(background.getPreferredSize());
+        layeredPane.add(background, Integer.valueOf(0));
+    }
+
+    // - - - M O U S E L I S T E N E R  M E T H O D S - - - //
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // gets the component pressed
+        Object object = e.getSource();
+
+        // if the object is a JLabel
+        if (object instanceof JLabel temp) {
+            // if the JLabel being entered has a name
+            // (all JLabels with mouseListeners added should be named)
+            if (temp.getName() != null) {
+                String name = temp.getName();
+                // if that JLabel is a background label (the sky, water, or ground)
+                if (name.equals("Sky") || name.equals("Water") || name.equals("Ground")) {
+                    // TODO: open the action menu correlated with the label
+                }
+                else if (name.equals("Next Day Label")) {
+                    // TODO: go to next day
+                }
+
+            }
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // gets the component pressed
+        Object object = e.getSource();
+
+        // if the object is a JLabel
+        if (object instanceof JLabel temp) {
+            // if the JLabel being entered has a name
+            // (all JLabels with mouseListeners added should be named)
+            if (temp.getName() != null) {
+                temp.setBorder(new LineBorder(Color.YELLOW, 1));
+            }
+        }
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // gets the component pressed
+        Object object = e.getSource();
+
+        // if the object is a JLabel
+        if (object instanceof JLabel temp) {
+            // removes the border after exiting
+            temp.setBorder(null);
         }
     }
 }
