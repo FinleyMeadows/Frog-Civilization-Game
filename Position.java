76300db014c4833c9a8fdbea566ec7fcf08cf3a1
FinleyMeadows <finleytmeadows@gmail.com>
2024-12-10 12:17:29 -0800

    Created the Position class

--- Position.java
+++ Position.java
@@ -1,2 +1,13 @@
 public class Position {
+
+    private int xPos;
+    private int yPos;
+    // rise / run {rise, run}
+    private int[] slope = new int[2];
+    // 'n', 'e', 's', 'w'
+    private char direction;
+
+    public Position() {
+
+    }
 }
